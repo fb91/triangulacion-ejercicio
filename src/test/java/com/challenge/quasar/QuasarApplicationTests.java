247716c6f27a00b5c187a128a60ae4d674372ff2
Fabricio Bianchi <fabriciob91@gmail.com>
2021-09-01 18:54:49 -0300

    Add some unit tests

--- src/test/java/com/challenge/quasar/QuasarApplicationTests.java
+++ src/test/java/com/challenge/quasar/QuasarApplicationTests.java
@@ -1,13 +1,48 @@
 package com.challenge.quasar;
 
+import com.challenge.quasar.entities.Location;
+import com.challenge.quasar.entities.Satellite;
+import com.challenge.quasar.services.LocationService;
+import com.challenge.quasar.services.MessageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class QuasarApplicationTests {
 
+	@Autowired
+	private LocationService locationService;
+
+	@Autowired
+	private MessageService messageService;
+
 	@Test
-	void contextLoads() {
+	public void checkLocation() throws Exception {
+		List<Satellite> satelliteList = new ArrayList<Satellite>();
+		satelliteList.add(new Satellite("sato", 142.7, null));
+		satelliteList.add(new Satellite("skywalker", 115.5, null));
+		satelliteList.add(new Satellite("kenobi", 100, null));
+		Location calculatedLocation = locationService.getLocation(satelliteList);
+		Location expectedLocation = new Location(new double[]{-58.3, -69.6});
+		assertEquals(expectedLocation.getX(), calculatedLocation.getX());
+		assertEquals(expectedLocation.getY(), calculatedLocation.getY());
+	}
+
+	@Test
+	public void checkMessage() throws Exception {
+		List<String[]> messageList = new ArrayList<String[]>();
+		messageList.add(new String[] {"Contenido", "", "mensaje", "", "prueba"});
+		messageList.add(new String[] {"", "del", "", "", ""});
+		messageList.add(new String[] {"", "", "", "de", ""});
+		String decodedMessage = messageService.decodeMessage(messageList);
+		String expectedMessage = "Contenido del mensaje de prueba";
+		assertEquals(decodedMessage, expectedMessage);
 	}
 
 }
