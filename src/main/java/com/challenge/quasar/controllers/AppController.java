447f562791e9727c7b8910d12be09203425bc734
Fabricio Bianchi <fabriciob91@gmail.com>
2021-09-01 01:01:23 -0300

    Add support to cacheable splitted satellite info

--- src/main/java/com/challenge/quasar/controllers/AppController.java
+++ src/main/java/com/challenge/quasar/controllers/AppController.java
@@ -1,27 +1,39 @@
 package com.challenge.quasar.controllers;
 
+import com.challenge.quasar.cache.CacheConfig;
 import com.challenge.quasar.entities.Location;
+import com.challenge.quasar.entities.Satellite;
 import com.challenge.quasar.entities.SatelliteList;
 import com.challenge.quasar.entities.Spaceship;
 import com.challenge.quasar.services.LocationService;
 import com.challenge.quasar.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class AppController {
 
     private final LocationService locationService;
     private final MessageService messageService;
+    private final CacheConfig cache;
 
     @Autowired
-    public AppController(LocationService locationService, MessageService messageService) {
+    private Environment environment;
+
+    @Autowired
+    public AppController(LocationService locationService, MessageService messageService, CacheConfig cache) {
         this.locationService = locationService;
         this.messageService = messageService;
+        this.cache = cache;
     }
 
     @PostMapping(path = "/topsecret", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -34,6 +46,26 @@ public class AppController {
             String spaceshipMessage = messageService.decodeMessage(satelliteList.obtainMessages());
             Spaceship spaceship = new Spaceship(spaceshipLocation, spaceshipMessage);
             return ResponseEntity.status(HttpStatus.OK).body(spaceship);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+    }
+
+    @PostMapping(path = "/topsecret_split/{satellite_name}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity topSecretSplit(@PathVariable String satellite_name, @RequestBody Satellite satelliteInfo) {
+        try {
+            satelliteInfo.setName(satellite_name);
+            this.cache.cacheManager().getCache("satellitesCache").put(satelliteInfo.getName(), satelliteInfo);
+            String[] satellitesNames = this.environment.getProperty("satellites.names").split(",");
+            List<Satellite> cachedSatelliteList = new ArrayList<Satellite>();
+            for (String satelliteName : satellitesNames) {
+                Cache.ValueWrapper cache = this.cache.cacheManager().getCache("satellitesCache").get(satelliteName);
+                if (cache != null) {
+                    Satellite cachedSatellite = (Satellite) cache.get();
+                    cachedSatelliteList.add(cachedSatellite);
+                }
+            }
+            return this.topSecret(new SatelliteList(cachedSatelliteList));
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
