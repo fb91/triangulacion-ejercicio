cbc3579fc353fcc9c87fd0039db120fb2304b96d
Fabricio Bianchi <fabriciob91@gmail.com>
2021-09-01 22:35:11 -0300

    Fix decoded message length

--- src/main/java/com/challenge/quasar/services/MessageService.java
+++ src/main/java/com/challenge/quasar/services/MessageService.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 @Service
 public class MessageService {
     public String decodeMessage(List<String[]> messagesList) throws Exception {
-        String[] decodedMessage = new String[messagesList.stream().findFirst().get().length];
+        String[] decodedMessage = new String[_calculateMaxMessageLength(messagesList)];
         messagesList.forEach( (final String[] message) -> {
             for (int i=0; i<message.length; i++) {
                 if (!message[i].isBlank()) {
@@ -21,5 +21,15 @@ public class MessageService {
             throw new Exception("No se logró decodificar por completo el mensaje");
         }
         return String.join(" ", decodedMessage);
+    }
+
+    private int _calculateMaxMessageLength(List<String[]> messagesList) {
+        int maxMessageLength = 0;
+        for (String[] message : messagesList) {
+            if (message.length > maxMessageLength) {
+                maxMessageLength = message.length;
+            }
+        }
+        return maxMessageLength;
     }
 }
